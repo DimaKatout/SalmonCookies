7fbb88ed535a973107c500f1c9f33d5f33befcc2
DimaKatout <dimakatout97@gmail.com>
2020-03-07 13:26:13 +0200

    wrong numbers

--- js/app.js
+++ js/app.js
@@ -1,16 +1,20 @@
 'use strict'
 var loc= [];
 var locName=[];
+var tot=[];
+var arrr=[];
+var hours=['6a.m','7a.m','8a.m','9a.m','10a.m','11a.m','12p.m','1p.m','2p.m','3p.m','4p.m','5p.m','6p.m','7p.m'];
+
 var container = document.getElementById("d");
 
 var tableEl=document.createElement('table');
 container.appendChild(tableEl);
 
-var seattle=new Location('Seattle',23,65,6.3);
+/*var seattle=new Location('Seattle',23,65,6.3);
 var tokyo=new Location("Tokyo",3,24,1.2);
 var dubai=new Location("Dubai",11,38,3.7);
 var paris=new Location("Paris",20,38,2.3);
-var lima=new Location("Lima",2,16,4.6);
+var lima=new Location("Lima",2,16,4.6);*/
 
 
 
@@ -20,8 +24,8 @@ function Location(locationName,minHourlyCus,maxHourlyCus,avgCokPerCust)
     this.maxHourlyCus=maxHourlyCus;
     this.avgCokPerCust=avgCokPerCust;
     this.arr=[];
-    this.arrr=[];
-    this.tot=[];
+    //this.arrr=[];
+    //this.tot=[];
     this.sum=0;
 this.totalSum=0;
 loc.push(this);
@@ -31,7 +35,8 @@ locName.push(locationName);
 
 }
 
-Location.prototype.numberOfCustPerHour=function(){var num = Math.floor(Math.random() * (this.maxHourlyCus
+Location.prototype.numberOfCustPerHour=function()
+{var num = Math.floor(Math.random() * (this.maxHourlyCus
     - this.minHourlyCus)) + this.minHourlyCus;
 //alert(num);
 return num;}
@@ -77,13 +82,14 @@ Location.prototype.createList=function()
 
 Location.prototype.calculateSum=function()
 {// var sum = 0;
-    this.arrr = [];
-    for (var i = 6; i < 12; i++) {
+    //this.arrr = [];
+    /*for (var i = 6; i < 12; i++) {
         var numCokies = this.cokPurchasedPerHour()
         this.sum = this.sum + numCokies;
         //alert(i + 'am:' + numCokies + 'cookies');
         //this.arrr.push(`${i} am: ${numCokies} cookies`);
-        this.arrr.push(numCokies);
+        //this.arrr.push(numCokies);
+        arrr.push(numCokies);
 
         
         
@@ -92,7 +98,8 @@ Location.prototype.calculateSum=function()
         var numCokies = this.cokPurchasedPerHour();
         this.sum =this. sum + numCokies;
         //this.arrr.push(`${i} pm: ${numCokies} cookies`);
-        this.arrr.push(numCokies);
+        //this.arrr.push(numCokies);
+        arrr.push(numCokies);
 
         
         
@@ -102,7 +109,8 @@ Location.prototype.calculateSum=function()
         var numCokies = this.cokPurchasedPerHour();
         this.sum = this.sum + numCokies;
         //this.arrr.push(`${i} pm: ${numCokies} cookies`);
-        this.arrr.push(numCokies);
+        //this.arrr.push(numCokies);
+        arrr.push(numCokies);
 
 
         
@@ -110,9 +118,30 @@ Location.prototype.calculateSum=function()
     }
     
     
-    this.arrr.push(this.sum);
+   // this.arrr.push(this.sum);
+   arrr.push(this.sum);
 
-return this.arrr;}
+//return this.arrr;
+return arrr;*/
+for (var i = 0; i < hours.length; i++) {
+    var numCokies = this.cokPurchasedPerHour();
+    this.sum = this.sum + numCokies;
+    //this.arrr.push(`${i} pm: ${numCokies} cookies`);
+    //this.arrr.push(numCokies);
+    arrr.push(numCokies);
+    
+    
+    
+    
+    
+}
+
+
+// this.arrr.push(this.sum);
+arrr.push(this.sum);
+
+//return this.arrr;
+return arrr;}
     //alert(this.arrr);}
 
     Location.prototype.sales=function()
@@ -124,11 +153,11 @@ return this.arrr;}
     containerr.appendChild(ullEl);
    
     this.calculateSum(); 
-    for (var i = 0; i < this.arrr.length; i++) {
-        console.log("i  ", i, "   ", this.arrr[i]);
+    for (var i = 0; i <arrr.length; i++) {
+        //console.log("i  ", i, "   ", arrr[i]);
         var liiEl = document.createElement("li");
         ullEl.appendChild(liiEl);
-        liiEl.textContent = this.arrr[i];
+        liiEl.textContent = arrr[i];
         console.log(liiEl);
         
     }}
@@ -136,18 +165,20 @@ return this.arrr;}
     
 
     Location.prototype.totalSalesPerHour=function()
-    {  this.arrr
+    {  //this.arrr
         for (var i = 1; i < 16; i++) {
            
-            this.totalSum = this.totalSum + this.arrr[i];
+            this.totalSum = this.totalSum + arrr[i];
             //alert(i + 'am:' + numCokies + 'cookies');
             //this.arrr.push(`${i} am: ${numCokies} cookies`);
-            this.tot.push(this.totalSum);
+            //this.tot.push(this.totalSum);
+            tot.push(this.totalSum);
     
             
             
         }
-        return this.tot;
+       // return this.tot;
+       return tot;
     }
         
         
@@ -164,43 +195,19 @@ return this.arrr;}
     dataEl.appendChild(tdE0);
 
     tdE0.textContent=this.locationName;
-
-
     var array=this.calculateSum();
-for(let i=0;i<array.length;i++)
-{
-var tdEl=document.createElement('td');
-tdEl.textContent=array[i];
-dataEl.appendChild(tdEl);
-
-
-}
-
-
-
-
-}
-/*var dataE2=document.createElement('td');
-    tableEl.appendChild(dataE2);
-    //var tdE2=document.createElement('td');
-tdE2.textContent=this.sum;
-dataE2.appendChild(tdE2);*/
-
-
-
-    
-
+    for(let i=0;i<array.length;i++)
+    {
+    var tdEl=document.createElement('td');
+    tdEl.textContent=array[i];
+    dataEl.appendChild(tdEl);
     
     
+    }
+    }
 
 
-
-/*var container = document.getElementById("d");
-
-var tableEl=document.createElement('table');
-container.appendChild(tableEl);*/
-
-
+   
 
 //had lazem a3melo be function
 
@@ -235,36 +242,90 @@ tableEl.appendChild(trEl);
 var tddEl=document.createElement('td');
 trEl.appendChild(tddEl);
 tddEl.textContent="Total";
-/*for(let i=0;i<Location.tot.length;i++)
+for(let i=0;i<tot.length;i++)
 {
 var tdE3=document.createElement('td');
-tdE3.textContent=Location.tot[i];
+tdE3.textContent=tot[i];
+trEl.appendChild(tdE3);
+
+}
+for (var i = 0; i < hours.length; i++)
+{var summation=0;
+for(let j=0;j<arrr.length;j++)
+{summation=summation+arrr[i];
+
+
+
+}
+var tdE4=document.createElement('td');
+//tdE4.textContent=array[i];
+tdE4.textContent=summation;
+
+trEl.appendChild(tdE4);
+
+}
+
+
+
+/*for(let i=0;i<tot.length;i++)
+{
+var tdE3=document.createElement('td');
+tdE3.textContent=tot[i];
 trEl.appendChild(tdE3);
 
 }*/
-/* 
-for (let j=0;j<arrr.length;i++)
-var sum=0;
-{
-for (let j=0;j<shops.length;j++)
-{sum=sum+shops[i].salesPerHour[i]; 
+/*for (var i = 6; i < 12; i++)
+{var summation=0;
+for(let j=0;j<arrr.length;j++)
+{summation=summation+arrr[i];
+
+
+
 }
-var tdE1=create td
-trE1.app
-tdE1.text=sum;
-//back to part 2 min 27 lab 8
-}
+var tdE4=document.createElement('td');
+//tdE4.textContent=array[i];
+tdE4.textContent=summation;
+
+trEl.appendChild(tdE4);
+
 }
 
 
- */
+for (var i = 12; i < 13; i++)
+{var summation=0;
+for(let j=0;j<arrr.length;j++)
+{summation=summation+arrr[i];
+}
+var tdE5=document.createElement('td');
+//tdE4.textContent=array[i];
+tdE5.textContent=summation;
+
+trEl.appendChild(tdE5);}
+
+for (let i=1;i<8;i++)
+{var summation=0;
+    for(let j=0;j<arrr.length;j++)
+    {summation=summation+arrr[i];
+    }
+    var tdE6=document.createElement('td');
+    //tdE4.textContent=array[i];
+    tdE6.textContent=summation;
+    
+    trEl.appendChild(tdE6);}*/
+
 
 }
+
 
 
 renderHeader();
 
 
+var seattle=new Location('Seattle',23,65,6.3);
+var tokyo=new Location("Tokyo",3,24,1.2);
+var dubai=new Location("Dubai",11,38,3.7);
+var paris=new Location("Paris",20,38,2.3);
+var lima=new Location("Lima",2,16,4.6);
 
 
 for(let i=0;i<loc.length;i++){
@@ -273,8 +334,8 @@ for(let i=0;i<loc.length;i++){
     loc[i].render();
     loc[i].totalSalesPerHour();}
 
+    
+   
     renderFooter();
-
-
-
+    
 
